e9c242aac3d871294247471bf7ab34b33c646741
khalilhamdii <khaalil.hamdi@gmail.com>
2020-12-10 13:21:33 +0100

    Add bootstrap files and dependencies

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import _ from "lodash";
-import "bootstrap";
-import "./style.scss";
+import "./assets/bootstrap/css/bootstrap.min.css";
+import "./assets/bootstrap/js/bootstrap.min.js";
+function requireAll(r) {
+  r.keys().forEach(r);
+}
+requireAll(require.context("./assets/js/", true, /\.js$/));
+function importAll(r) {
+  return r.keys().map(r);
+}
+
+const images = importAll(
+  require.context("./assets//img/", false, /\.(png|jpe?g|svg)$/)
+);
 
 function component() {
   const element = document.createElement("div");
 
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(["Hello", "webpack"], " ");
-
   return element;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+var webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const path = require("path");
@@ -5,6 +6,10 @@ const path = require("path");
 module.exports = {
   entry: "./src/index.js",
   plugins: [
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+    }),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: "Restaurant Page",
@@ -17,25 +22,12 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(scss)$/,
-        use: [
-          {
-            // Adds CSS to the DOM by injecting a `<style>` tag
-            loader: "style-loader",
-          },
-          {
-            // Interprets `@import` and `url()` like `import/require()` and will resolve them
-            loader: "css-loader",
-          },
-          {
-            // Loader for webpack to process CSS with PostCSS
-            loader: "postcss-loader",
-          },
-          {
-            // Loads a SASS/SCSS file and compiles it to CSS
-            loader: "sass-loader",
-          },
-        ],
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
       },
     ],
   },
